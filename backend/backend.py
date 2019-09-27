9141d8ef5919c669e1782fd23577e570aafd6f52
Séverin Lemaignan <severin@guakamole.org>
2019-09-27 10:50:08 +0000

    More robust backend

--- backend/backend.py
+++ backend/backend.py
@@ -19,11 +19,14 @@ maze = []
 width = 0
 height = 0
 
+STARTPOS=[1,1]
+MAXLIFE = 10
+MIN_TIME_BETWEEN_INTERACTIONS=0.2 #seconds
+
 robots = {
-        "wall-e gerard": [10, 10],
-        "john": [1, 1],
-        "eve": [10, 7],
-        "wall-e2": [5, 1]
+        "wall-e gerard": {"pos":[10, 10], "lastinteraction":0, "life":10},
+        "eve": {"pos":[10, 7], "lastinteraction":0, "life":10},
+        "john": {"pos":[1, 1], "lastinteraction":0, "life":10}
         }
 
 def store_map(mapdata):
@@ -34,7 +37,7 @@ def store_map(mapdata):
     width = rawmaze["width"]
     height = rawmaze["height"]
 
-    maze = [1 if x in [399,431,463,492,493,494,495] else 0 for x in rawmaze["data"]] 
+    maze = [True if x in [399,431,463,492,493,494,495] else False for x in rawmaze["data"]] 
 
     for j in range(height):
         for i in range(width):
@@ -47,26 +50,30 @@ def store_map(mapdata):
 
     logger.info("Maze successfully loaded!")
 
+def is_map_loaded():
+    return width and height
+
 def get_obstacles(x,y):
 
     # obstacle at centre, north, south, east, west?
-    obstacles = [False, False, False, False, False]
+    obstacles = [True, True, True, True, True]
 
     if x >= 0 and y >= 0 and x < width and y < height and maze[x + y * width]:
-        obstacles[0] = True
+        obstacles[0] = False
 
     if x >= 0 and y-1 >= 0 and x < width and y-1 < height and maze[x + (y-1) * width]:
-        obstacles[1] = True
+        obstacles[1] = False
 
     if x >= 0 and y+1 >= 0 and x < width and y+1 < height and maze[x + (y+1) * width]:
-        obstacles[2] = True
+        obstacles[2] = False
 
     if x+1 >= 0 and y >= 0 and x+1 < width and y < height and maze[x+1 + y * width]:
-        obstacles[3] = True
+        obstacles[3] = False
 
     if x-1 >= 0 and y >= 0 and x-1 < width and y < height and maze[x-1 + y * width]:
-        obstacles[4] = True
+        obstacles[4] = False
 
+    logger.info(str(obstacles))
     return obstacles
 
 def set_robot(name, x, y):
@@ -77,26 +84,49 @@ def set_robot(name, x, y):
         logger.info("Can not place robot there!")
         return json.dumps(False)
 
-    robots[name] = [x,y]
+    robots[name]["pos"] = [x,y]
     return json.dumps(True)
 
 def get_robot(name):
     if name not in robots:
         return json.dumps([-1,-1])
-    return json.dumps(robots[name])
+    return json.dumps(robots[name]["pos"])
 
 def get_robots():
-    robots["eve"][0] += 1
-    return json.dumps(robots)
+    robots["eve"]["pos"][0] += 1
 
+    res = {}
+    for name,v in robots.items():
+        res[name] = v["pos"]
+
+    return json.dumps(res)
+
+
+def create_new_robot(name):
+    logger.info("Placing new robot %s at start position" % name)
+    robots[name] = {"pos": STARTPOS,
+                    "lastinteraction": time.time(),
+                    "life": MAXLIFE
+                    }
 
 def move(name, direction):
-    if name not in robots:
+    if not is_map_loaded():
+        logger.error("Map not loaded yet! Reload webpage.")
         return json.dumps([False,[]])
+
+    if name not in robots:
+        create_new_robot(name)
 
     logger.info("Moving robot %s to %s" % (name,direction))
 
-    x,y = robots[name]
+    now = time.time()
+    if now - robots[name]["lastinteraction"] < MIN_TIME_BETWEEN_INTERACTIONS:
+        logger.error("Too many interactions with %s. Wait a bit." % name)
+        return json.dumps([False,[]])
+
+    robots[name]["lastinteraction"] = now
+
+    x,y = robots[name]["pos"]
 
     if direction == 'N':
         nx, ny = x, y-1
@@ -110,10 +140,12 @@ def move(name, direction):
     c,n,s,e,w = get_obstacles(nx,ny)
 
     if c:
+        logger.info("...can not move there!")
+        robots[name]["life"] -= 1
         return json.dumps([False,[]])
 
     else:
-        robots[name] = [x,y]
+        robots[name]["pos"] = [nx,ny]
         return json.dumps([True,[n,s,e,w]])
 
 def app(environ, start_response):
@@ -144,10 +176,10 @@ def app(environ, start_response):
             return get_robots()
         if "set" in options:
             name,x,y = json.loads(options["set"][0])
-            set_robot(name,x,y)
+            return set_robot(name,x,y)
         if "move" in options:
             name,direction = json.loads(options["move"][0])
-            move(name,direction)
+            return move(name,direction)
 
         return ""
 
