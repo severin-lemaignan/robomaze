fbd5c10228ecb14732b263e7acc412584ca2c9ef
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2019-10-03 13:42:21 +0100

    Random_walk: stop when dead

--- scripts/random_walk.py
+++ scripts/random_walk.py
@@ -7,11 +7,10 @@ import time
 import json
 import requests
 
-URL = "https://robomaze.skadge.org/api?"
-
+REST_ENDPOINT = "https://robomaze.skadge.org/api?"
 
 if len(sys.argv) != 2:
-    print("Usage: random.py <name of robot>")
+    print("Usage: random_walk.py <name of robot>")
     sys.exit()
 
 name = sys.argv[1]
@@ -22,11 +21,18 @@ next_directions = ["N", "S", "E", "W"]
 starttime = time.time()
 
 try:
+
     while 1:
 
-        response = requests.get(URL + "move=" + json.dumps([name, random.choice(next_directions)]))
-        obstacles = json.loads(response.text)[1]
-        next_directions = [x for x in ["N", "S", "E", "W"] if not obstacles[["N", "S", "E", "W"].index(x)]]
+        result = requests.get(REST_ENDPOINT + "move=" + json.dumps([name, random.choice(next_directions)]))
+        print(json.loads(result.text))
+
+        life = requests.get(REST_ENDPOINT + "life=" + json.dumps(name))
+        if json.loads(life.text) == 0:
+            print("Dead :-(")
+            break
+
         time.sleep(0.25)
+
 finally:
     print("Total elapsed time: %d seconds" %(time.time() - starttime))
