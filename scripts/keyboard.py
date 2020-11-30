11ad7b7e3ffaca7be9a8e21306ecb379a2b320c6
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-11-30 17:31:29 +0000

    scripts/keyboard: py3 + small fixes

--- scripts/keyboard.py
+++ scripts/keyboard.py
@@ -1,11 +1,17 @@
 #! /usr/bin/env python
 
-import sys, select, tty, termios
+import sys, select, tty, termios,time
 
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 old_settings = termios.tcgetattr(sys.stdin)
+
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
 
 try:
     tty.setcbreak(sys.stdin.fileno())
@@ -16,21 +22,28 @@ try:
     while 1:
         if isData():
             c = sys.stdin.read(1)
-            key = ord(c.decode('utf-8'))
+            key = ord(c)
             if c == '\x1b':         # x1b is ESC
-                arrow = ord(sys.stdin.read(2)[1].decode('utf-8'))
+                arrow = ord(sys.stdin.read(2)[1])
                 if arrow == 65: # up 
                     v += 0.1
                 elif arrow == 66: # bottom
                     v -= 0.1
                 elif arrow == 67: # right 
-                    w += 0.1
-                elif arrow == 68: # left 
                     w -= 0.1
+                elif arrow == 68: # left 
+                    w += 0.1
+                else: # attention: due to the read(2), need to press esc 3 times to get there!
+                    print("Exiting")
+                    break
                 print("v=%.1f, w=%.1f" % (v,w))
 
             else:
-                print(key)
+                if c == "q":
+                    print("Exiting")
+                    break
+
+        time.sleep(0.01)
 
 finally:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
